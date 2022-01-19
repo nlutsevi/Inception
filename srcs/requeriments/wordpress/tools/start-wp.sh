c1647508e532f9e4820ca28c1513d022ebc2967e
Natasha Lutsevich <natashalutsevich@MacBook-Air-de-Natasha.local>
2022-01-19 05:03:16 +0100

    env_vars_wp

---
+++ srcs/requeriments/wordpress/tools/start-wp.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sed "s/db_name/$DB_NAME /var/www/html/wordpress/wp-config.php
+sed "s/db_user/$DB_USER /var/www/html/wordpress/wp-config.php
+sed "s/db_password/$DB_PASSWORD /var/www/html/wordpress/wp-config.php
--- srcs/requeriments/wordpress/tools/wp-config.php
+++ srcs/requeriments/wordpress/tools/wp-config.php
@@ -1,1 +1,33 @@
 #https://kinsta.com/es/blog/wp-config-php/
+<?php
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'db_name');
+
+/** MySQL database username */
+define('DB_USER', 'db_user');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'db_password');
+
+/** MySQL hostname */
+define('DB_HOST', 'mariadb:3306');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+define('AUTH_KEY',		'put your unique phrase here');
+define('SECURE_AUTH_KEY',	'put your unique phrase here');
+define('LOGGED_IN_KEY',		'put your unique phrase here');
+define('NONCE_KEY',		'put your unique phrase here');
+define('AUTH_SALT',		'put your unique phrase here');
+define('SECURE_AUTH_SALT',	'put your unique phrase here');
+define('LOGGED_IN_SALT',	'put your unique phrase here');
+define('NONCE_SALT',		'put your unique phrase here');
+
+$table_prefix  = 'wp_';
+
+/* That's all, stop editing! Happy blogging. */
