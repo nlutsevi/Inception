234af24d26a86515e5b133c37be9bee8660bfa96
nlutsevi <nlutsevi@debian.nlutsevi.42.fr>
2022-01-28 21:32:15 +0100

    openrc softlevel error

--- srcs/requeriments/mariadb/tools/run_mariadb.sh
+++ srcs/requeriments/mariadb/tools/run_mariadb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mysql --user=$MYSQL_DB_USER --password=$MYSQL_DB_ROOT_PASSWORD $MYSQL_DB_NAME
+mysqld --user=mysql --datadir=/var/lib/mysql --password=$MYSQL_DB_ROOT_PASSWORD $MYSQL_DB_NAME
 
 
