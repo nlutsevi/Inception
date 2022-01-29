ab3a1352d03855b67813ab0b8548b0273261ca18
Natasha Lutsevich <natashalutsevich@MacBook-Air-de-Natasha.local>
2022-01-29 23:07:21 +0100

    openrc error fixed

--- scripts/rm_vol.sh
+++ scripts/rm_vol.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 
 docker volume rm srcs_wp_vol
+docker volume rm srcs_db_vol
 docker volume prune
 docker system prune
--- srcs/requeriments/mariadb/tools/run_mariadb.sh
+++ srcs/requeriments/mariadb/tools/run_mariadb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-mysqld --user=mysql --datadir=/var/lib/mysql --password=$MYSQL_DB_ROOT_PASSWORD $MYSQL_DB_NAME
+#mysqld --user=mysql --datadir=/var/lib/mysql --password=$MYSQL_DB_ROOT_PASSWORD $MYSQL_DB_NAME
+
+/bin/usr/mysqld --user=mysql --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --pid-file=/run/mysqld/mariadb.pid --port=3306 --log-error=/var/log/mysqld.log --log-warnings=4 --skip-networking=0
+
 
 
