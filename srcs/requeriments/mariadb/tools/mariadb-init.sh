12ddf3666f0fba5b1ec156c9bdad8aabbf841f12
Natasha Lutsevich <natashalutsevich@MacBook-Air-de-Natasha.local>
2022-01-31 05:20:37 +0100

    Table 'mysql.db' doesn't exist error

--- srcs/requeriments/mariadb/tools/mariadb-init.sh
+++ srcs/requeriments/mariadb/tools/mariadb-init.sh
@@ -7,7 +7,7 @@ touch /run/openrc/softlevel
 /etc/init.d/mariadb setup
 rc-service mariadb start
 
-# Create configuration file
+# Create root and normal user for our database
 cat << EOF > /tools/init.sql
 CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '$MYSQL_DB_ROOT_PASSWORD';
 CREATE DATABASE IF NOT EXISTS $MYSQL_DB_DATABASE;
@@ -17,9 +17,9 @@ GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_DB_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 
-# Create Wordpress database
 mysql < tools/init.sql
 
+# Add worldpressdb.sql to our database
 mysql -u root --password=$MYSQL_DB_ROOT_PASSWORD $MYSQL_DB_DATABASE < tools/wordpressdb.sql
 
 # Secure mariadb database server
--- srcs/requeriments/mariadb/tools/run_mariadb.sh
+++ srcs/requeriments/mariadb/tools/run_mariadb.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-#mysqld --user=mysql --datadir=/var/lib/mysql --password=$MYSQL_DB_ROOT_PASSWORD $MYSQL_DB_NAME
+#mysqld --user=mysql --datadir=/var/lib/mysql --port=3306 --plugin-dir=/usr/lib/mariadb/plugin #--log-error=/var/log/mysqld.log
 
-/bin/usr/mysqld --user=mysql --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --pid-file=/run/mysqld/mariadb.pid --port=3306 --log-error=/var/log/mysqld.log --log-warnings=4 --skip-networking=0
+# mysqld --print-defaults
 
+chmod 755 /var/lib/mysql
 
+mysqld --user=mysql --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --pid-file=/run/mysqld/mariadb.pid --port=3306 --log-error=/var/log/mysqld.log --log-warnings=4 --skip-networking=0
+
+# mysql_install_db –-user=mysql --ldata=/var/lib/mysql
+
+# mysql_install_db --defaults-file=~/.my.cnf
 
