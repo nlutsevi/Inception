dd44e4bf00c1764a96e9d93c97dacec99e56152c
Natasha Lutsevich <natallia.lutsevich2@gmail.com>
2022-02-03 06:33:07 +0100

    error encontrado - nombre de volumenes

--- srcs/requeriments/mariadb/tools/mariadb-init.sh
+++ srcs/requeriments/mariadb/tools/mariadb-init.sh
@@ -14,28 +14,20 @@ rc-service mariadb start
 # sed "s/root_pass/$MYSQL_ROOT_PASSWORD/" tools/init.sql
 
 cat << EOF > /tools/init.sql
-CREATE USER 'root'@'%' IDENTIFIED BY 'root_pass';
-CREATE DATABASE mariadb;
-CREATE USER 'nati@'%' IDENTIFIED BY 'pass';
-GRANT ALL ON mariadb.* TO 'nati'@'%';
-GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root_pass';
+
+cat << EOF > /tools/init.sql
+CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE DATABASE $MYSQL_DATABASE;
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
-
-# cat << EOF > /tools/init.sql
-# CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-# CREATE DATABASE $MYSQL_DATABASE;
-# CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-# GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-# GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-# FLUSH PRIVILEGES;
-# EOF
 
 mysql < tools/init.sql
 
 # Add worldpressdb.sql to our database
-# mysql -u root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < tools/wordpressdb.sql
-mysql -u root --password='root_pass' 'mariadb' < tools/wordpressdb.sql
+mysql -u root --password=$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < tools/wordpressdb.sql
 
 
 #Secure mariadb database server
--- srcs/requeriments/wordpress/tools/wp-config.php
+++ srcs/requeriments/wordpress/tools/wp-config.php
@@ -1,33 +1,45 @@
-#https://kinsta.com/es/blog/wp-config-php/
 <?php
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'db_name');
+	// ** MySQL settings - You can get this info from your web host ** //
+	/** The name of the database for WordPress */
 
-/** MySQL database username */
-define('DB_USER', 'db_user');
+	define('DB_NAME', 'db_name');
 
-/** MySQL database password */
-define('DB_PASSWORD', 'db_password');
+	/** MySQL database username */
+	define('DB_USER', 'db_user');
 
-/** MySQL hostname */
-define('DB_HOST', 'mariadb:3306');
+	/** MySQL database password */
+	define('DB_PASSWORD', 'db_password');
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+	/** MySQL hostname */
+	define('DB_HOST', 'mariadb:3306');
 
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+	/** Database Charset to use in creating database tables. */
+	define('DB_CHARSET', 'utf8');
 
-define('AUTH_KEY',		'put your unique phrase here');
-define('SECURE_AUTH_KEY',	'put your unique phrase here');
-define('LOGGED_IN_KEY',		'put your unique phrase here');
-define('NONCE_KEY',		'put your unique phrase here');
-define('AUTH_SALT',		'put your unique phrase here');
-define('SECURE_AUTH_SALT',	'put your unique phrase here');
-define('LOGGED_IN_SALT',	'put your unique phrase here');
-define('NONCE_SALT',		'put your unique phrase here');
+	/** The Database Collate type. Don't change this if in doubt. */
+	define('DB_COLLATE', '');
 
-$table_prefix  = 'wp_';
+	define('AUTH_KEY',		'put your unique phrase here');
+	define('SECURE_AUTH_KEY',	'put your unique phrase here');
+	define('LOGGED_IN_KEY',		'put your unique phrase here');
+	define('NONCE_KEY',		'put your unique phrase here');
+	define('AUTH_SALT',		'put your unique phrase here');
+	define('SECURE_AUTH_SALT',	'put your unique phrase here');
+	define('LOGGED_IN_SALT',	'put your unique phrase here');
+	define('NONCE_SALT',		'put your unique phrase here');
 
-/* That's all, stop editing! Happy blogging. */
+	define('WP_DEBUG', true);
+
+	define('WP_DEBUG_LOG', true);
+
+	$table_prefix  = 'wp_';
+
+	/** Absolute path to the WordPress directory. */
+	if ( !defined('ABSPATH') )
+	        define('ABSPATH', dirname(__FILE__) . '/');
+	
+	/** Sets up WordPress vars and included files. */
+	require_once(ABSPATH . 'wp-settings.php');
+
+
+	/* That's all, stop editing! Happy blogging. */
